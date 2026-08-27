79cb976ced3a8c29c2aa7ed1e13af33e185fa91a
Abhishek <guptaabhishek27538@gmail.com>
2026-08-27 19:58:26 +0530

    Exception handling concept added in memory

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,41 @@
+﻿using ExceptionHandling;
+
+   
+        //Exceptiondemo exceptiondemo = new Exceptiondemo();
+        //exceptiondemo.AssignmentOfArray();
+        UserExceptionHand obj = new UserExceptionHand();
+        obj.Withdraw();
+
+    
+
+
+// exception handling is a technique to handle the run time error in program to avoid the abnormal termination
+// in exception handling we have 2 type of exception first is built-in exception and 2nd is user-defined exception
+class Exceptiondemo
+{
+    int[] array = new int[5];
+   
+    public void AssignmentOfArray()
+    {
+        try
+        {
+            array[0] = 1;
+            array[3] = 5;
+            array[6] = 10;                                          // this is built-in exceotion 
+            // indexoutOfRange exception 
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e);
+        }
+        finally
+        {
+            Console.WriteLine("This is finally block");
+        }
+    }
+
+}
+
+
+
+
---
+++ ExceptionHandling/UserExceptionHand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionHandling
+{
+    class UserExceptionHand
+    {   
+        public int password; public int balance ;
+        public UserExceptionHand() 
+        {
+            password = 1234;
+            balance = 1000;
+        }
+        public void Withdraw()
+        {
+            try
+            {
+                Console.WriteLine("Enter your password : ");
+                int pass = Convert.ToInt32(Console.ReadLine());
+                if (pass == password)
+                {
+                    Console.WriteLine("password is correct");
+                    Console.WriteLine("Enter the amount to withdraw : ");
+                    int amount = Convert.ToInt32(Console.ReadLine());
+                    try
+                    {
+                        if (amount < balance)
+                        {
+                            Console.WriteLine($"Your withdraw amount is : {amount}");
+                            balance -= amount;
+                            Console.WriteLine($"Your remaining balance is : {balance}");
+                        }
+                        throw new Exception("Insufficient funds");
+                    }
+                    finally
+                    {
+                        Console.WriteLine("Finally block executed");
+                    }
+
+
+
+                }
+                throw new Exception("Invalid password");
+            }
+            finally
+            {
+                Console.WriteLine("Finally block executed");
+            }
+        }
+    }
+}
